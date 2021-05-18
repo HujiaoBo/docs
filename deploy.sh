013ca91167f0e69f124f30ab77a2f17596b73675
hujiaobo <350086484@qq.com>
2021-05-18 16:17:35 +0800

    deloy

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.iyarn docs:devo
+# git push -f git@github.com:mischieff/blog.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:HujiaoBo/docs.git master:gh-pages
+
+cd -
